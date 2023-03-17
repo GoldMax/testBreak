876cb1bb39e2df9697603bd9df783eed7516019c
GoldMax <goldmax3000@gmail.com>
2023-03-17 15:11:20 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace testBreak
+{
+	class Program
+	{
+		[DllImport("libc", SetLastError = true, EntryPoint = "kill")]
+		private static extern int sys_kill(int pid, int sig);
+  private static readonly int SIGINT = 2;
+
+		static void Main(string[] args)
+		{
+			bool isWin = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+			Debug.WriteLine("OS  : " + (isWin ? "Windows" : "Linux"));
+			Debug.WriteLine("Args: " + (args.Length > 0 ? String.Join(' ', args) : ""));
+
+			try
+			{
+				using Process proc = new Process();
+				proc.StartInfo.UseShellExecute = false;
+				proc.StartInfo.FileName = "/code/signaler/signaler";
+				proc.StartInfo.Arguments = String.Join(' ', args);
+
+				proc.Start();
+				Console.WriteLine();
+    Console.WriteLine($"PID={proc.Id} started. Press Enter to exit");
+				Console.ReadLine();
+
+				sys_kill(proc.Id, SIGINT);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
